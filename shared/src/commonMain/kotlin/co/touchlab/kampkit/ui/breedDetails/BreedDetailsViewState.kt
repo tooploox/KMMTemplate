9523100cbe624c4502d1be9e8b6f3e0e4ef9515f
bpedryc <b.pedryc11@gmail.com>
2023-07-03 10:18:03 +0200

    Fix Ktlint errors

--- shared/src/commonMain/kotlin/co/touchlab/kampkit/ui/breedDetails/BreedDetailsViewState.kt
+++ shared/src/commonMain/kotlin/co/touchlab/kampkit/ui/breedDetails/BreedDetailsViewState.kt
@@ -3,7 +3,7 @@ package co.touchlab.kampkit.ui.breedDetails
 data class BreedDetailsViewState(
     val breed: BreedDisplayable = BreedDisplayable(),
     val error: String? = null,
-    val isLoading: Boolean = false,
+    val isLoading: Boolean = false
 ) {
     companion object {
         fun default() = BreedDetailsViewState()
